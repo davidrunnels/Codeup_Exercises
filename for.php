50f5d2eb536140407d903c81bd632ede72833eef
David Runnels <david.runnels@gmail.com>
2014-11-24 17:54:40 -0600

    refactored

--- for.php
+++ for.php
@@ -1,16 +1,10 @@
 <?php
 
-echo "I'm going to count for you.  You'll give me two numbers.\n";
+echo "\nI'm going to count for you.  You'll give me two numbers.\n";
 
-echo "Then you'll tell me what increment I should use to count.\n";
-
-fwrite(STDOUT, "Enter the starting number: ");
-
-$start_number = trim(fgets(STDIN));
+echo "Then you'll tell me what increment I should use to count.\n\n";
 
 do {
-
-	echo "Please enter an interger.\n";
 
 	fwrite(STDOUT, "Enter the starting number: ");
 
@@ -18,13 +12,7 @@ do {
 
 } while (!is_numeric($start_number));
 
-fwrite(STDOUT, "Enter the ending number: ");
-
-$end_number = trim(fgets(STDIN));
-
 do {
-
-	echo "Please enter an ending number.\n";
 
 	fwrite(STDOUT, "Enter the ending number: ");
 
@@ -32,13 +20,7 @@ do {
 
 } while (!is_numeric($end_number));
 
-fwrite(STDOUT, "Choose an increment to use to count: ");
-
-$increment_number = trim(fgets(STDIN));
-
 do {
-
-	echo "Please enter an interger.\n";
 
 	fwrite(STDOUT, "Enter an increment to use to count: ");
 
