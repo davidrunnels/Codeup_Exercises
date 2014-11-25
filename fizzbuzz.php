51c3484b59e3764ae00e0a1fc8d3439d9c8ba21a
David Runnels <david.runnels@gmail.com>
2014-11-25 10:56:12 -0600

    refactored

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,29 +1,18 @@
 <?php
 
 
-for ($i = 1; $i <= 100; $i++)
-
-{
+for ($i = 1; $i <= 100; $i++) {
+	
 	if($i % 3 == 0 && $i % 5 ==0){
-
 		echo "FizzBuzz\n";
 
-	}
-
-	else if($i % 3 == 0){
-
+	} else if($i % 3 == 0){
 		echo "Fizz\n";
 
-	}
-
-	else if($i % 5 == 0){
-
+	} else if($i % 5 == 0){
 		echo "Buzz\n";
 
-	}
-
-	else {
-
+	} else {
 		echo $i."\n";
 
 	}
