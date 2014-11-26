fe0db586e9f35349d65d409dde77c3a07dcec911
David Runnels <david.runnels@gmail.com>
2014-11-26 10:03:42 -0600

    added switch exercise

---
+++ switch.php
@@ -0,0 +1,43 @@
+<?php
+
+
+ // Set the default timezone
+ date_default_timezone_set('America/Chicago');
+
+ // Get Day of Week as number
+ // 1 (for Monday) through 7 (for Sunday)
+ $dayOfWeek = date('N');
+
+ switch($dayOfWeek) {
+     case 1:
+         // Output Monday
+     	echo "It's Monday.";
+     	break;
+     case 2:
+         // Output Tuesday
+     	echo "It's Winedown Tuesday!" . PHP_EOL;
+     	break;
+     case 3:
+     	echo "It's Food Truck Wednesday!" . PHP_EOL;
+     	break;
+     // etc through day 7
+     case 4:
+     	echo "It's Thursday." . PHP_EOL;
+     	break;
+     case 5:
+     	echo "It's Friday!" . PHP_EOL;
+     	break;
+     case 6:
+     	echo "It's Saturday." . PHP_EOL;
+     	break;
+     case 7:
+     	echo "Easy like a Sunday morning." . PHP_EOL;
+     	break;
+
+// Test results
+ }
+
+ if ($dayOfWeek = 7) {
+ 	echo "Test line:  It's Food Truck Wednesday!";
+
+ }
