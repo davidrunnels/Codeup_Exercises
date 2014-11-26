1fbad302d44947dcf09e58c8a759e05d0bbf2737
David Runnels <david.runnels@gmail.com>
2014-11-26 13:51:56 -0600

    added code to call each function

--- arithmetic.php
+++ arithmetic.php
@@ -15,3 +15,8 @@ function multiply($a, $b) {
 function divide($a, $b) {
     echo $a / $b;
 }
+
+echo add(5, 4) . PHP_EOL;
+echo subtract(6, 4) . PHP_EOL;
+echo multiply(3, 1) . PHP_EOL;
+echo divide(16,4) . PHP_EOL;
