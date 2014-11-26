7ed24f0d21799c209af86be04a922f7af5fa1d0a
David Runnels <david.runnels@gmail.com>
2014-11-26 14:47:09 -0600

    added code to validate arguments

--- arithmetic.php
+++ arithmetic.php
@@ -1,28 +1,48 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b;
+	if (is_numeric($a) && is_numeric($b)) {
+    	echo $a + $b;
+} else {
+        echo "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 function subtract($a, $b) {
-    echo $a - $b;
+	if (is_numeric($a) && is_numeric($b)) {
+    	echo $a - $b;
+} else {
+        echo "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 function multiply($a, $b) {
-    echo $a * $b;
+    if (is_numeric($a) && is_numeric($b)) {
+    	echo $a * $b;
+} else {
+        echo "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 function divide($a, $b) {
-    echo $a / $b;
+    if (is_numeric($a) && is_numeric($b)) {
+    	echo $a / $b;
+} else {
+        echo "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 function modulus($a, $b) {
-	echo $a % $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a % $b;
+} else {
+        echo "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 echo add(5, 4) . PHP_EOL;
 echo subtract(6, 4) . PHP_EOL;
 echo multiply(3, 1) . PHP_EOL;
 echo divide(16, 4) . PHP_EOL;
-
+echo modulus(19, 4) . PHP_EOL;
 
