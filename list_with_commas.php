b675a198886c61a30d9938a066da6a5e77b285b0
David Runnels <david.runnels@gmail.com>
2014-12-03 10:36:58 -0600

    add explode

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,7 +1,10 @@
 <?php
 
+
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
-$exploded = explode(', ', $physicistsString);
+$famousFakePhysicists = explode(', ', $physicistsString);
 
-var_dump($exploded);
+var_dump($famousFakePhysicists);
+
+ echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists[0]}, {$famousFakePhysicists[1]}, {$famousFakePhysicists[2]}, {$famousFakePhysicists[3]}, {$famousFakePhysicists[4]} {$famousFakePhysicists[5]}.";
