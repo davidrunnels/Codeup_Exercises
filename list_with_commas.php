587e0351b68f65f5404a0345aa630019284e9a1e
David Runnels <david.runnels@gmail.com>
2014-12-03 11:24:26 -0600

    add funtcion humanizeList and sort

--- list_with_commas.php
+++ list_with_commas.php
@@ -3,8 +3,38 @@
 
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
-$famousFakePhysicists = explode(', ', $physicistsString);
 
-var_dump($famousFakePhysicists);
+// // Converts array into list n1, n2, ..., and n3
+function humanizedlist($string, $sorting = false) {
+  // Your solution goes here!
+		$array = explode(', ', $string);
+		// print_r($array);
 
- echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists[0]}, {$famousFakePhysicists[1]}, {$famousFakePhysicists[2]}, {$famousFakePhysicists[3]}, {$famousFakePhysicists[4]} {$famousFakePhysicists[5]}.";
+		if($sorting){
+			natcasesort($array);
+			// print_r($array);
+		}
+		
+		$last_item = array_pop($array);
+		//$new_last_item = 'and '. $last_item;
+		//array_push($array, $new_last_item);
+		
+		$new_string = implode(', ', $array);
+		
+		return "{$new_string} and {$last_item}";
+	
+}
+// function sorting_option($array) {
+// 	return natcasesort($array);
+// }
+
+// sorting_option($famousFakePhysicists);
+// Humanize that list
+$famousFakePhysicists = humanizedlist($physicistsString, true);
+
+// Update your code to list the physicists by first name, in alphabetical order
+
+  
+// Output sentence
+echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
+
